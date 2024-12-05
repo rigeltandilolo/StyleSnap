30452bca26c82c38efd393758379c7004e1827d0
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-05 12:57:52 +0800

    Create PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import { manifestForPlugIn } from './manifest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [react(), VitePWA({
+    devOptions: {
+      enabled: true
+    },
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.js',
+    registerType: 'autoUpdate',
+    injectManifest: {
+      swDest: 'dist/sw.js'
+    },
+    manifest: {
+      name: "StyleSnap",
+      short_name: "StyleSnap",
+      description: "Virtual wardrobe app",
+      icons: [{
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable-icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      }
+      ],
+      theme_color: '#171717',
+      background_color: '#f0e7db',
+      display: "standalone",
+      scope: '/',
+      start_url: "/",
+      orientation: 'portrait'
+    }
+  })],
 })
