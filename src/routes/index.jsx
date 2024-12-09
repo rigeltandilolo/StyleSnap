7bc5d2e719bf398a18340624c5350948892db0ea
rigeltandilolo <ijengrigel@gmail.com>
2024-12-09 09:56:42 +0800

    komit 1 - Login Page
    tampilan login page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import LoginScreen from '../pages/LoginScreen'; // Pastikan path sudah sesuai
+import Home from '../pages/Home'; // Pastikan path sudah sesuai
 
 export const router = createBrowserRouter([
   {
+    path: '/LoginScreen',
+    element: <LoginScreen />, // Halaman LoginScreen
+  },
+  {
     path: '/',
-    element: <Home />
-  }
-])
+    element: <Home />, // Halaman Home
+  },
+]);
