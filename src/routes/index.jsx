92971c412f57d02d8d35f99af00b308514e58ace
rigeltandilolo <ijengrigel@gmail.com>
2024-12-10 08:59:25 +0800

    Onboarding Page
    sudah ter-direct ke halaman login saat tekan tombol 'Get Started'

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import LoginScreen from '../pages/LoginScreen'; // Pastikan path sudah sesuai
-import Home from '../pages/Home'; // Pastikan path sudah sesuai
+import OnboardingScreen from '../pages/OnboardingPage';
+import LoginScreen from '../pages/LoginScreen';
+import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
+    path: '/OnboardingPage',
+    element: <OnboardingScreen />, //onboarding
+  },
+  {
     path: '/LoginScreen',
-    element: <LoginScreen />, // Halaman LoginScreen
+    element: <LoginScreen />, //LoginScreen
   },
   {
     path: '/',
-    element: <Home />, // Halaman Home
+    element: <Home />, //Home
   },
 ]);
