676e64f41267568fc7fbc1d893a6bd498661c90f
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-06 14:22:39 +0800

    Adjust routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import LoginScreen from '../pages/LoginScreen'; // Pastikan path sudah sesuai
-import Home from '../pages/Home'; // Pastikan path sudah sesuai
+import LoginScreen from '../pages/LoginScreen';
+import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
-    path: '/LoginScreen',
-    element: <LoginScreen />, // Halaman LoginScreen
+    path: '/',
+    element: <Home />,
   },
   {
-    path: '/',
-    element: <Home />, // Halaman Home
+    path: '/login',
+    element: <LoginScreen />,
   },
-]);
+])
