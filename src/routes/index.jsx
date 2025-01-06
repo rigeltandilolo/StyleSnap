66fcf4d2e88924ebb5d436d37d508de32fd6af76
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-06 14:29:11 +0800

    Remove unused folder and adjust routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import OnboardingScreen from '../pages/OnboardingPage';
-import LoginScreen from '../pages/LoginScreen';
 import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
-    path: '/OnboardingPage',
-    element: <OnboardingScreen />, //onboarding
-  },
-  {
-    path: '/LoginScreen',
-    element: <LoginScreen />, //LoginScreen
-  },
-  {
     path: '/',
-    element: <Home />, //Home
+    element: <Home />,
+  },
+  {
+    path: '/onboarding',
+    element: <OnboardingScreen />,
   },
 ]);
