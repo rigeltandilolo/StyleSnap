02a69ba89e895dd4f0dcd828fca3fb1a75e6a7d6
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-16 23:00:43 +0800

    Add https config

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,70 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), VitePWA({
-    devOptions: {
-      enabled: true,
-      type: 'module'
-    },
-    strategies: 'injectManifest',
-    srcDir: 'src',
-    filename: 'sw.js',
-    registerType: 'autoUpdate',
-    injectManifest: {
-      swDest: 'dist/sw.js'
-    },
-    manifest: {
-      name: "StyleSnap",
-      short_name: "StyleSnap",
-      description: "Virtual wardrobe app",
-      icons: [{
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any'
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+  const HOST = process.env.VITE_HOST
+
+  return defineConfig({
+    plugins: [react(), VitePWA({
+      devOptions: {
+        enabled: true,
+        type: 'module'
       },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any'
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      registerType: 'autoUpdate',
+      injectManifest: {
+        swDest: 'dist/sw.js'
       },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/maskable-icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+      manifest: {
+        name: "StyleSnap",
+        short_name: "StyleSnap",
+        description: "Virtual wardrobe app",
+        icons: [{
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: '/maskable-icon.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+        ],
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
       }
-      ],
-      theme_color: '#171717',
-      background_color: '#f0e7db',
-      display: "standalone",
-      scope: '/',
-      start_url: "/",
-      orientation: 'portrait'
+    })],
+    server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, `./ssl/${HOST}-key.pem`)),
+        cert: fs.readFileSync(path.resolve(__dirname, `./ssl/${HOST}.pem`))
+      },
+      host: '0.0.0.0'
     }
-  })],
-})
+  })
+}
